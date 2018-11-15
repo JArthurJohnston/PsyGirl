2a169d9a713fd783aa1ea5f20538a7b7c51eec62
JArthurJohnston <joe.a.johnston@gmail.com>
2018-11-14 21:11:05 -0500

    attempting to improve the camera controls

--- Assets/Scripts/Player/CameraMovement.cs
+++ Assets/Scripts/Player/CameraMovement.cs
@@ -7,7 +7,14 @@ public class CameraMovement : MonoBehaviour {
 	public GameObject playerBody;
 
 	void Update () {
-        var movement = Input.GetAxis("Horizontal2") * speed * Time.deltaTime;
-		transform.RotateAround(playerBody.transform.position, Vector3.up, movement);
+        var horizontalMovement = Input.GetAxis("Horizontal2") * speed * Time.deltaTime;
+        var verticalMovement = Input.GetAxis("Vertical2") * speed * Time.deltaTime;
+
+		transform.RotateAround(playerBody.transform.position, Vector3.up, horizontalMovement);
+		transform.RotateAround(playerBody.transform.position, Vector3.right, verticalMovement);
+		
+		Debug.Log("Looking At: " + playerBody.transform.position);
+		Camera.main.transform.LookAt(playerBody.transform.position);
+		// transform.LookAt(playerBody.transform.position);
 	}
 }
