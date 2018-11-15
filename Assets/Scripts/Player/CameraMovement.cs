b5bb01fb92b8bea72d6efbffa8425e760f17c735
JArthurJohnston <joe.a.johnston@gmail.com>
2018-11-15 07:25:32 -0500

    fixed the camera movement and got it moving on both axis

--- Assets/Scripts/Player/CameraMovement.cs
+++ Assets/Scripts/Player/CameraMovement.cs
@@ -3,18 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraMovement : MonoBehaviour {
-	public float speed;
+	public float horizontalSeed;
+	public float verticalSpeed;
+	public float maxHeight;
+	public float minHeight;
 	public GameObject playerBody;
 
 	void Update () {
-        var horizontalMovement = Input.GetAxis("Horizontal2") * speed * Time.deltaTime;
-        var verticalMovement = Input.GetAxis("Vertical2") * speed * Time.deltaTime;
+        var horizontalMovement = Input.GetAxis("Horizontal2") * horizontalSeed * Time.deltaTime;
+        var verticalMovement = Input.GetAxis("Vertical2") * verticalSpeed * Time.deltaTime;
 
-		transform.RotateAround(playerBody.transform.position, Vector3.up, horizontalMovement);
-		transform.RotateAround(playerBody.transform.position, Vector3.right, verticalMovement);
-		
-		Debug.Log("Looking At: " + playerBody.transform.position);
+		transform.RotateAround(playerBody.transform.position, transform.up, horizontalMovement);
+		updateCameraHeight(verticalMovement);
 		Camera.main.transform.LookAt(playerBody.transform.position);
-		// transform.LookAt(playerBody.transform.position);
+	}
+
+	void updateCameraHeight(float delta){
+		transform.Translate(Vector3.up * delta);
+		transform.position = new Vector3(transform.position.x, cameraHeight(), transform.position.z); 
+	}
+
+	float cameraHeight(){
+		return Mathf.Clamp(transform.position.y, minHeight, maxHeight);
 	}
 }
