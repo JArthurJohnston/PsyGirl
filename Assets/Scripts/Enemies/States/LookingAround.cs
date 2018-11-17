f1fbf91421539512db09e1a32298d1f77cb87a57
JArthurJohnston <joe.a.johnston@gmail.com>
2018-11-17 07:42:50 -0500

    got the enemies to look around a bit, though its jerky

--- Assets/Scripts/Enemies/States/LookingAround.cs
+++ Assets/Scripts/Enemies/States/LookingAround.cs
@@ -6,7 +6,7 @@ using UnityEngine.AI;
 public class LookingAround : EnemyState {
     static Quaternion ninetyDegrees = Quaternion.Euler(0, 90, 0);
 
-    float turningSpeed = 0.15f;
+    public float turningSpeed;
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         base.OnStateEnter(animator, stateInfo, layerIndex);
@@ -14,18 +14,21 @@ public class LookingAround : EnemyState {
     }
 
     IEnumerator lookAround(Animator animator){
+        agent.enabled = false;
         yield return new WaitForSeconds(1f);
         rotate(animator, ninetyDegrees);
         yield return new WaitForSeconds(1f);
         rotate(animator, ninetyDegrees);
         yield return new WaitForSeconds(1f);
         rotate(animator, ninetyDegrees);
+        agent.enabled = true;
         animator.SetBool(ARRIVED_AT_WAYPOINT, false);
     }
 
     void rotate(Animator animator, Quaternion degrees){
-            var rotation  = animator.gameObject.transform.rotation * degrees; // this adds a 90 degrees Y rotation
-            animator.gameObject.transform.rotation = 
-                Quaternion.Slerp(animator.gameObject.transform.rotation, rotation, Time.deltaTime * turningSpeed);
+        Debug.Log("rotating");
+        var rotation  = animator.gameObject.transform.rotation * degrees; // this adds a 90 degrees Y rotation
+        animator.gameObject.transform.rotation = 
+            Quaternion.Slerp(animator.gameObject.transform.rotation, rotation, turningSpeed);
 	}
 }
