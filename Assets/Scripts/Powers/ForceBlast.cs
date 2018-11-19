dbfc9f4f8b50d0642dffb0763db46686c57cfa75
JArthurJohnston <joe.a.johnston@gmail.com>
2018-11-19 13:03:07 -0500

    started on a force blast power

---
+++ Assets/Scripts/Powers/ForceBlast.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForceBlast : MonoBehaviour {
+
+	public float ExpansionSpeed;
+	public float TimeToLive;
+
+	// Use this for initialization
+	void Start () {
+		Destroy(gameObject, TimeToLive);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var expanded = Time.deltaTime * ExpansionSpeed;
+		transform.localScale += new Vector3(expanded, expanded, expanded);
+	}
+}
