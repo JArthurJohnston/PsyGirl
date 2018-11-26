332670da79f8239aa5810e0bcd41e1a32dab96d6
JArthurJohnston <joe.a.johnston@gmail.com>
2018-11-26 12:47:26 -0500

    started on a pointer controller to allow the player to select an item theyre looking at

---
+++ Assets/Scripts/Player/ForceHoldController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForceHoldController : AbstractPowerController
+{
+    public override void Handle(float input)
+    {
+    }
+
+    public override void Initialize()
+    {
+    }
+}
---
+++ Assets/Scripts/Player/PointerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointerController : MonoBehaviour {
+
+	public float lookOffset;
+
+	public GameObject pointer;
+
+	void FixedUpdate () {
+		var playerTransform = Player.Main.transform;
+		var ray = new Ray(playerTransform.position + (playerTransform.up * lookOffset), Camera.main.transform.forward);
+
+		Debug.DrawRay(playerTransform.position + (playerTransform.up * lookOffset), Camera.main.transform.forward, Color.green);
+		RaycastHit hit;
+
+        if(Physics.Raycast(ray, out hit, 100f)){
+			Debug.DrawLine(playerTransform.position + (playerTransform.up * lookOffset), hit.transform.position, Color.cyan);
+			pointer.transform.position = hit.transform.position;
+        }
+		
+	}
+}
