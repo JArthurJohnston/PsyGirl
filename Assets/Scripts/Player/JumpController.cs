506bb30f8da6e5cd9be5d7406afef3f20e30c202
JArthurJohnston <joe.a.johnston@gmail.com>
2018-11-30 07:39:24 -0500

    removed the default light and started playing around with spot lights and emissive materials

--- Assets/Scripts/Player/JumpController.cs
+++ Assets/Scripts/Player/JumpController.cs
@@ -1,18 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
 
 public class JumpController : MonoBehaviour {
 
 	public float jumpSpeed;
 	private bool isJumping;
+	private bool Jumped;
 
 	void Start(){
 		isJumping = false;
 	}
 	
 	void Update () {
-		if(Input.GetAxis("Jump") > 0){
+		if(CrossPlatformInputManager.GetAxis("Jump") > 0){
 			if(!isJumping){
 				var rotation = transform.rotation;
 				GetComponent<Rigidbody>().velocity = Vector3.up * jumpSpeed;
