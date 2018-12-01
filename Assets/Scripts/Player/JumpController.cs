67ddafada60c99ffa465d9e68aac83ac9af16b72
JArthurJohnston <joe.a.johnston@gmail.com>
2018-12-01 14:16:10 -0500

    mapped the crouch button to a new input

--- Assets/Scripts/Player/JumpController.cs
+++ Assets/Scripts/Player/JumpController.cs
@@ -9,16 +9,22 @@ public class JumpController : MonoBehaviour {
 	private bool isJumping;
 	private bool Jumped;
 
+	private float floorHeight;
+
 	void Start(){
 		isJumping = false;
+		Jumped = false;
 	}
 	
 	void Update () {
 		if(CrossPlatformInputManager.GetAxis("Jump") > 0){
 			if(!isJumping){
+				Debug.Log("Jumped");
+				// floorHeight = transform.position.y;
 				var rotation = transform.rotation;
 				GetComponent<Rigidbody>().velocity = Vector3.up * jumpSpeed;
 				transform.rotation = rotation;
+				// Jumped = true;
 				isJumping = true;
 			}
 		} else {
