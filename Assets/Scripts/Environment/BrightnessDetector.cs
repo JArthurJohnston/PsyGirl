5d40349b991a6fec48014abc1a46493e9d6e7b44
JArthurJohnston <joe.a.johnston@gmail.com>
2018-12-04 18:54:50 -0500

    started on a brightness detector

---
+++ Assets/Scripts/Environment/BrightnessDetector.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrightnessDetector : MonoBehaviour {
+
+	private GameObject[] lights;
+
+	// Use this for initialization
+	void Start () {
+		lights = GameObject.FindGameObjectsWithTag("Light Source");
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		foreach (var eachLight in lights)
+		{
+			var distanceToPlayer = Vector3.Distance(eachLight.transform.position, Player.Main.transform.position);
+		}
+	}
+}
