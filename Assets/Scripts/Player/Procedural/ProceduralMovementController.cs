a6414f652045a0ccfa60274747fdb1ac2f8af823
JArthurJohnston <joe.a.johnston@gmail.com>
2018-12-13 12:58:05 -0500

    got the movement controller to position the feet properly

--- Assets/Scripts/Player/Procedural/ProceduralMovementController.cs
+++ Assets/Scripts/Player/Procedural/ProceduralMovementController.cs
@@ -6,16 +6,32 @@ public class ProceduralMovementController : MonoBehaviour {
 
     public float turningSpeed;
     public GameObject characterBody;
-    public float jumpThreashold = 1;
-
+    public float jumpThreashold = 1f;
     private Rigidbody _rigidbody;
+    public float feetAngle = 5f;
+    public Transform RightFoot;
+    public Transform LeftFoot;
+
+    public float MaxLegLength = 4f;
+    private int SkipPlayerMask;
+
+
     void Start(){
+        // RightFoot = transform.Find("RFoot");
+        // LeftFoot = transform.Find("LFoot");
+        SkipPlayerMask = RayHelper.allButLayerMask("Player");
+        var threshold = FindStandingThreashold();
+        if(threshold > 0){
+            jumpThreashold = threshold + 0.25f;
+        }
         _rigidbody = GetComponent<Rigidbody>();
     }
 
     public void MovePlayer(Vector3 movement, float jump){
+
         var isOnTheGround = IsGrounded();
         RotatePlayer(movement);
+        PositionFeet();
         if(jump > 0 && isOnTheGround){
             _rigidbody.velocity = JumpInCurrentDirection(jump);
         }else if(isOnTheGround){
@@ -30,13 +46,36 @@ public class ProceduralMovementController : MonoBehaviour {
 		}
 	}
 
+    private void PositionFeet(){
+        RightFoot.position = FindPosition(Quaternion.AngleAxis(feetAngle, characterBody.transform.forward) * characterBody.transform.up);
+        LeftFoot.position = FindPosition(Quaternion.AngleAxis(feetAngle, -characterBody.transform.forward) * characterBody.transform.up);
+    }
+
+    private Vector3 FindPosition(Vector3 direction){
+        RaycastHit hit;
+        var rightRay = new Ray(transform.position, direction);
+        Debug.DrawRay(transform.position, direction, Color.cyan);
+        if(Physics.Raycast(rightRay, out hit, MaxLegLength, SkipPlayerMask)){
+            return hit.point;
+        }
+        return direction * MaxLegLength;;
+    }
+
 	private bool IsGrounded(){
 		var ray = new Ray(gameObject.transform.position, Vector3.down);
-		RaycastHit hit;
 		return Physics.Raycast(ray, jumpThreashold);
 	}
 
     private Vector3 JumpInCurrentDirection(float jump){
         return new Vector3(_rigidbody.velocity.x, jump, _rigidbody.velocity.z);
+    }
+
+    private float FindStandingThreashold(){
+		var ray = new Ray(gameObject.transform.position, Vector3.down);
+		RaycastHit hit;
+		if(Physics.Raycast(ray, out hit, 4)){
+            return Vector3.Distance(gameObject.transform.position, hit.point);;
+        }
+        return -1f;
     }
 }
